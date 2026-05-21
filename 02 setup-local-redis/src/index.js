5e27a84d2644f85d5cef75ab71df1063eb811d01
Ritik Patel <ritikpatel82993@gmail.com>
2026-05-21 13:08:36 +0530

    Added: 02 setup-local-redis

---
+++ 02 setup-local-redis/src/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import  Redis  from "ioredis";
+import mongoose from "mongoose";
+
+const app = express();
+
+const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
+
+app.get("/redis", async(req,res)=>{
+    const reply = await redis.ping();
+    res.json({redis: reply})
+})
+
+app.get("/mongo", async(req,res)=>{
+    const url = process.env.MONGODB_URL || 'mongodb://localhost:27017/practice_mongo';
+
+    if(mongoose.connection.readyState === 0){
+        await mongoose.connect(url);
+    }
+    res.json({mongo: 'connected' , database:mongoose.connection.name});
+})
+
+app.listen(3000, ()=>{
+    console.log("server is running on port: 3000")
+})
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Setup command
+
+1. After writing the docker-compose.yml file run the below command 
+   docker compose up
